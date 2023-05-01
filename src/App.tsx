ea3858d6634217f0ab08861b4223015b5c6a97fb
Gaoridang <monooi@naver.com>
2023-05-01 15:34:35 +0900

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
 import { useState } from 'react';
 import GameGrid from './assets/components/GameGrid';
+import GameHeading from './assets/components/GameHeading';
 import GenreList from './assets/components/GenreList';
 import NavBar from './assets/components/NavBar';
 import PlatformSelector from './assets/components/PlatformSelector';
@@ -42,18 +43,21 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
-          <Flex paddingLeft={2} marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatform={gameQuery.platform}
-                onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
+          <Box paddingLeft={2}>
+            <GameHeading gameQuery={gameQuery} />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  selectedPlatform={gameQuery.platform}
+                  onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
+                />
+              </Box>
+              <SortSelector
+                sortOrder={gameQuery.sortOrder}
+                onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}
               />
-            </Box>
-            <SortSelector
-              sortOrder={gameQuery.sortOrder}
-              onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}
-            />
-          </Flex>
+            </Flex>
+          </Box>
           <GameGrid gameQuery={gameQuery} />
         </GridItem>
       </Grid>
